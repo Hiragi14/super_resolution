5599d2d9c0dcc7af724204d61ab9b8a0b4753fd1
kawai <kawai.syusei.s0@ist.hokudai.ac.jp>
2025-07-14 01:32:13 +0900

    [modify] train generator

--- train_srgan.py
+++ train_srgan.py
@@ -8,32 +8,32 @@ from lightning.pytorch.loggers import CSVLogger
 DIR_LOGS= './logs'
 MODEL_NAME = 'srgan_lightning'
 MAXEPOCH = 300
-SCALE = 4
+SCALE = 2
 
 def main():
-    generator = Generator()
+    generator = Generator(upscale_factor=SCALE)
     discriminator = Discriminator()
     data = DIV2KDataModule(data_dir='/ldisk/DeepLearning/Dataset/DIV2K/', batch_size=16, scale=SCALE, num_workers=4)
     
-    # # ---- Generator Training ----
-    # g_train_model = GeneratorLightningModule(generator=generator)
-    # trainer = pl.Trainer(
-    #     logger=CSVLogger(DIR_LOGS, name=MODEL_NAME),
-    #     accelerator="gpu",
-    #     devices=1,
-    #     max_epochs=200,
-    #     check_val_every_n_epoch=1,
-    #     precision=16,  # Enable mixed precision training
-    # )
-    # trainer.fit(g_train_model, datamodule=data)
-    # save_path = f"{DIR_LOGS}/{MODEL_NAME}/g_pre_weights.pth"
-    # torch.save(g_train_model.generator.state_dict(), save_path)
-    g_model = torch.load("./logs/srgan_lightning/g_pre_weights.pth")
-    generator.load_state_dict(g_model)
+    # ---- Generator Training ----
+    g_train_model = GeneratorLightningModule(generator=generator)
+    trainer = pl.Trainer(
+        logger=CSVLogger(DIR_LOGS, name=MODEL_NAME),
+        accelerator="gpu",
+        devices=1,
+        max_epochs=200,
+        check_val_every_n_epoch=1,
+        precision=16,  # Enable mixed precision training
+    )
+    trainer.fit(g_train_model, datamodule=data)
+    save_path = f"{DIR_LOGS}/{MODEL_NAME}/g_pre_weights.pth"
+    torch.save(g_train_model.generator.state_dict(), save_path)
+    # g_model = torch.load("./logs/srgan_lightning/version_5/g_pre_weights.pth")
+    # generator.load_state_dict(g_model)
     
     # ---- SRGAN Training ----
-    # model = SRGANLightningModule(generator=g_train_model.generator, discriminator=discriminator)
-    model = SRGANLightningModule(generator=generator, discriminator=discriminator)
+    model = SRGANLightningModule(generator=g_train_model.generator, discriminator=discriminator)
+    # model = SRGANLightningModule(generator=generator, discriminator=discriminator)
     trainer = pl.Trainer(
         logger=CSVLogger(DIR_LOGS, name=MODEL_NAME),
         accelerator="gpu",
